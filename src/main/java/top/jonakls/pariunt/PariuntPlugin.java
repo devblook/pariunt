476734dcab0c2c39bdf10bcca9dc1535c390d0b9
jonakls <asmot54@hotmail.com>
2024-12-15 00:23:44 -0500

    feat(command): add general commands of spawn system

--- src/main/java/top/jonakls/pariunt/PariuntPlugin.java
+++ src/main/java/top/jonakls/pariunt/PariuntPlugin.java
@@ -1,10 +1,19 @@
 package top.jonakls.pariunt;
 
+import dev.triumphteam.cmd.bukkit.BukkitCommandManager;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import top.jonakls.pariunt.command.PariuntCommand;
+
+import java.io.File;
 
 public class PariuntPlugin extends JavaPlugin {
+
+  private final BukkitCommandManager<CommandSender> commandManager = BukkitCommandManager.create(this);
+
   @Override
   public void onEnable() {
+    this.setupConfigurations();
     this.getLogger()
       .info("Pariunt plugin enabled");
   }
@@ -13,5 +22,22 @@ public class PariuntPlugin extends JavaPlugin {
   public void onDisable() {
     this.getLogger()
       .info("Pariunt plugin disabled");
+  }
+
+  private void setupConfigurations() {
+    final File config = new File(getDataFolder(), "config.yml");
+    if (!config.exists()) {
+      getConfig().options()
+        .copyDefaults(true);
+      saveConfig();
+    }
+  }
+
+  private void setupCommands() {
+    this.commandManager.registerCommand(new PariuntCommand());
+  }
+
+  public BukkitCommandManager<CommandSender> getCommandManager() {
+    return this.commandManager;
   }
 }
