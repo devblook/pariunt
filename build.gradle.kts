238ebbe0970de7854cc87cc1e7a3a2f440a352f4
jonakls <asmot54@hotmail.com>
2024-12-15 00:24:57 -0500

    build(script): add dependencies and configurations of building final jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
   id("java")
+  id("io.github.goooler.shadow") version "8.1.8"
 }
 
 repositories {
@@ -7,10 +8,15 @@ repositories {
     name = "papermc"
     url = uri("https://repo.papermc.io/repository/maven-public/")
   }
+  maven {
+    name = "triumph"
+    url = uri("https://repo.triumphteam.dev/snapshots/")
+  }
 }
 
 dependencies {
   compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+  implementation("dev.triumphteam:triumph-cmd-bukkit:2.0.0-SNAPSHOT")
 }
 
 tasks {
@@ -20,15 +26,17 @@ tasks {
     }
   }
 
+  shadowJar {
+    relocate("dev.triumphteam.cmd", "top.jonakls.pariunt.libs.cmd")
+
+    archiveFileName.set("${project.name}-${rootProject.version}.jar")
+  }
+
   withType<JavaCompile> {
     options.encoding = "UTF-8"
   }
 
   java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
-  }
-
-  jar {
-    archiveFileName.set("${project.name}-${rootProject.version}.jar")
   }
 }
